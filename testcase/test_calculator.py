8bff2366cbd436a56a42e886399bc881332632a0
koayyiting <93900494+koayyiting@users.noreply.github.com>
2024-01-18 17:11:33 +0800

    Update test_calculator.py

--- testcase/test_calculator.py
+++ testcase/test_calculator.py
@@ -1,6 +1,4 @@
 import sys
-print(sys.path)
-
 import unittest
 from sourcecode.calculator import add, subtract
 
@@ -11,6 +9,9 @@ class TestCalculator(unittest.TestCase):
 
     def test_subtract(self):
         self.assertEqual(subtract(5, 3), 2)
+
+    def test_add2(self):
+        self.assertEqual(add(3, 3), 6)
 
 if __name__ == '__main__':
     unittest.main()
